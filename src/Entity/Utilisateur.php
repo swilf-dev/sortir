223f858bf8c2b05810bbd3dd4de3652b2d745d88
Luana <luana.lpt@outlook.fr>
2026-04-28 09:47:32 +0200

    Ajout base de donnée utilisateur

---
+++ src/Entity/Utilisateur.php
@@ -0,0 +1,113 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\UtilisateurRepository;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+
+#[ORM\Entity(repositoryClass: UtilisateurRepository::class)]
+#[UniqueEntity('email')]
+#[UniqueEntity('pseudo')]
+class Utilisateur
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 180)]
+    private ?string $nom = null;
+
+    #[ORM\Column(length: 180)]
+    private ?string $prenom = null;
+
+    #[ORM\Column(name : 'pseudo', type: 'string', length: 180, unique: true)]
+    private ?string $pseudo = null;
+
+    #[ORM\Column(name : 'email', type: 'string', length: 180, unique: true)]
+    private ?string $email = null;
+
+    #[ORM\Column(length: 180)]
+    private ?string $mdp = null;
+
+    #[ORM\Column]
+    private ?int $numTel = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(string $nom): static
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    public function getPrenom(): ?string
+    {
+        return $this->prenom;
+    }
+
+    public function setPrenom(string $prenom): static
+    {
+        $this->prenom = $prenom;
+
+        return $this;
+    }
+
+    public function getPseudo(): ?string
+    {
+        return $this->pseudo;
+    }
+
+    public function setPseudo(string $pseudo): static
+    {
+        $this->pseudo = $pseudo;
+
+        return $this;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): static
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    public function getMdp(): ?string
+    {
+        return $this->mdp;
+    }
+
+    public function setMdp(string $mdp): static
+    {
+        $this->mdp = $mdp;
+
+        return $this;
+    }
+
+    public function getNumTel(): ?int
+    {
+        return $this->numTel;
+    }
+
+    public function setNumTel(int $numTel): static
+    {
+        $this->numTel = $numTel;
+
+        return $this;
+    }
+}
