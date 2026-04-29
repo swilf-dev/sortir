6a528a37fc9eaa49ee8b1070f15463f54a5fabf8
Luana <luana.lpt@outlook.fr>
2026-04-29 15:48:15 +0200

    Ajout des pages profils et de la barre de navigation

---
+++ src/Controller/ProfilController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Campus;
+use App\Entity\User;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class ProfilController extends AbstractController
+{
+    #[Route('/profil/{id}', name: 'app_profil', requirements: ['id' => '\d+'])]
+    public function index(User $user) : Response
+    {
+        return $this->render('profil/index.html.twig', [
+            'user' => $user,
+
+        ]);
+    }
+}
