958350c8faca2baf87f9edb139469bfa9dffe548
Jerome Hirth <jhirth@campus-eni.fr>
2026-04-30 17:29:30 +0200

    Correction des fixtures Sorties et App

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -11,44 +11,48 @@ use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Faker\Factory;
 class AppFixtures extends Fixture implements DependentFixtureInterface{
 
-private UserPasswordHasherInterface $passwordHasher;
+    private UserPasswordHasherInterface $passwordHasher;
    public function __construct(UserPasswordHasherInterface $passwordHasher)
     {
         $this->passwordHasher = $passwordHasher;
     }
 
+    public const int USER_COUNT = 30;
+
     public function load(ObjectManager $manager): void
     {
-    $faker = Factory::create('fr_FR');
-    for($i = 0; $i < 30; $i++) {
-        $user = new User();
-        $user->setEmail($faker->unique()->email());
-        $user->setPrenom($faker->firstName());
-        $user->setNom($faker->lastName());
-        $user->setPseudo($faker->unique()->userName());
-        $user->setNumTel($faker->phoneNumber(9));
-        $user->setRoles(['ROLE_USER']);
-        $user->setActif(true);
-        $randomCampus = $this->getReference('campus_' . $faker->numberBetween(0, 4), Campus::class);
-        $user->setCampus($randomCampus);
+        $faker = Factory::create('fr_FR');
+        for($i = 0; $i < self::USER_COUNT; $i++) {
+            $user = new User();
+            $user->setEmail($faker->unique()->email());
+            $user->setPrenom($faker->firstName());
+            $user->setNom($faker->lastName());
+            $user->setPseudo($faker->unique()->userName());
+            $user->setNumTel($faker->phoneNumber(9));
+            $user->setRoles(['ROLE_USER']);
+            $user->setActif(true);
+            $randomCampus = $this->getReference('campus_' . $faker->numberBetween(0, 4), Campus::class);
+            $user->setCampus($randomCampus);
 
 
 
-        $password = "test1234";
-        $hashedPassword =$this->passwordHasher->hashPassword(
-            $user,
-            $password
-        );
-        $user->setPassword($hashedPassword);
+            $password = "test1234";
+            $hashedPassword =$this->passwordHasher->hashPassword(
+                $user,
+                $password
+            );
+            $user->setPassword($hashedPassword);
 
 
-        $manager->persist($user);
-    }
-
-
+            $manager->persist($user);
+            $this->addReference('users_' . $i, $user);
+        }
 
         $manager->flush();
     }
+
+
+
     public function getDependencies(): array
     {
         return [
--- src/DataFixtures/SortieFixtures.php
+++ src/DataFixtures/SortieFixtures.php
@@ -2,17 +2,22 @@
 
 namespace App\DataFixtures;
 
-use App\Entity\Etat;
+//use App\Entity\Etat;
 use App\Entity\Sortie;
 use App\Entity\User;
 use App\Repository\EtatRepository;
 use App\Repository\UserRepository;
 use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
 use Doctrine\Persistence\ObjectManager;
 use Faker\Factory;
 
-class SortieFixtures extends Fixture
+class SortieFixtures extends Fixture implements DependentFixtureInterface
 {
+    public function getDependencies(): array
+    {
+        return [AppFixtures::class];
+    }
 
     public function __construct(
         private readonly UserRepository $userRepository,
@@ -22,60 +27,14 @@ class SortieFixtures extends Fixture
     public function load(ObjectManager $em): void
     {
         $faker = Factory::create('fr_FR');
-        //récupère les users pour en choisir un aléatoirement aux fixture de sortie
-        $users = $this->userRepository->findAll();
-        $user = $faker->randomElement($users);
 
-        //idem avec etat
         $etats = $this->etatRepository->findAll();
-        $etat = $faker->randomElement($etats);
-
-        $sortie = new Sortie();
-        $sortie->setOrganisateur($user);
-        $sortie->setNom("Sortie à la mer");
-        $sortie->setDateHeureDebut(new \DateTimeImmutable("now"));
-        $sortie->setDuree(7);
-        $sortie->setEtat($etat);
-        $sortie->setDateLimiteInscription(new \DateTimeImmutable("+30 days"));
-        $sortie->setNbInscriptionMax(10);
-        $sortie->setInfosSortie("rendez-vous devant le bar.");
-
-        //$sortie->setCampus("Nantes");
-        //$sortie->setLieu("Saint-tropez");
-
-        $em->persist($sortie);
-
-        $user1 = $faker->randomElement($users);
-        $sortie1 = new Sortie();
-        $sortie1->setOrganisateur($user1);
-        $sortie1->setNom("Sortie à la montagne");
-        $sortie1->setDateHeureDebut(new \DateTimeImmutable("now"));
-        $sortie1->setDuree(7);
-        $sortie1->setDateLimiteInscription(new \DateTimeImmutable("+30 days"));
-        $sortie1->setNbInscriptionMax(8);
-        $sortie1->setInfosSortie("rendez-vous devant le bar.");
-        //$sortie1->setCampus("Nantes");
-        //$sortie1->setLieu("Megève");
-        $em->persist($sortie1);
-
-        $user2 = $faker->randomElement($users);
-        $sortie2 = new Sortie();
-        $sortie2->setOrganisateur($user2);
-        $sortie2->setNom("Escape game php");
-        $sortie2->setDateHeureDebut(new \DateTimeImmutable(""));
-        $sortie2->setDuree(1);
-        $sortie2->setDateLimiteInscription(new \DateTimeImmutable("+30 days"));
-        $sortie2->setNbInscriptionMax(25);
-        $sortie2->setInfosSortie("rendez-vous devant le bar.");
-        //$sortie2->setCampus("Nantes");
-        //$sortie2->setLieu("Saint Herblain");
-        $em->persist($sortie2);
 
         for($i=1; $i<=10; $i++){
 
             $sortie = new Sortie();
-            $user = $faker->randomElement($users);
-            $sortie->setOrganisateur($user);
+            $index = $faker->numberBetween(0, AppFixtures::USER_COUNT - 1);
+            $sortie->setOrganisateur($this->getReference('users_' . $index, User::class));
             $sortie->setNom($faker->sentence($faker->numberBetween(3, 10)));
 
             $fk_dateHeureDebut = $faker->dateTimeBetween($startDate = '-30 days', $endDate = '+180 days', $timezone = null);
